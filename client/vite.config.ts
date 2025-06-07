189464ada6d64e3724ce00a37d9c7093945f497d
MacBook air_Szymek <szymek@MacBook-Air-Szymek.local>
2025-06-07 07:02:18 +0200

    vercel and vite.config.js edit 1.0

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,4 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()]});
+  plugins: [react()],
+	base: './',
+  build: {
+    outDir: 'dist',
+  },
+});
